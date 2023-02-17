92845dde8f465f3ad85d03dc232de855c00e2d60
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-17 21:21:49 +0100

    updated routes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,12 +2,17 @@ const express = require('express')
 
 const { notesDto } = require('../dtos/notes')
 const { validateUser } = require('../middlewares/authenticate.user')
-const { newNotes } = require('../controllers/note')
+const { protect } = require('../middlewares/validate.notes')
+const { newNotes, getMyNotes, getNote } = require('../controllers/note')
 const noteRouter = express.Router();
 
 noteRouter
     .route('/')
-    .post( validateUser, notesDto, newNotes )
+    .post( validateUser, newNotes )
+    .get( validateUser, getMyNotes )
 
+noteRouter
+    .route('/:id')
+    .get( validateUser, protect, getNote)
 
 module.exports = { noteRouter }

2367d5332ce80634c4736f15c96bcfd6d64a6753
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-17 22:51:12 +0100

    updated controllers

--- src/controllers/note.js
+++ src/controllers/note.js
@@ -1,8 +1,8 @@
 const { StatusCodes } = require('http-status-codes');
 const catchAsync = require('../errors/catchAsync')
-const { createNotes, getallNotes, getaNote } = require('../services/notes');
+const { createNotes, getallNotes, getaNote, deleteaNote, updateaNote } = require('../services/notes');
 const { AppError } = require('../errors/AppError');
-const { verifyNoteState } = require('../helper')
+const { verifyNoteState, buildQuery } = require('../helper')
 
 
 const newNotes = catchAsync( async( req, res ) => {
@@ -25,7 +25,8 @@ const newNotes = catchAsync( async( req, res ) => {
 const getMyNotes = catchAsync( async( req, res ) => {
 
     const owner = req.user;
-    const notes = await getallNotes( { owner: owner})
+    const query = buildQuery(owner, { ...req.query })
+    const notes = await getallNotes( query.query, query.sortBy )
 
     res.status( StatusCodes.OK ).json({
         status: true,
@@ -33,6 +34,7 @@ const getMyNotes = catchAsync( async( req, res ) => {
         notes
     })
 })
+
 
 const getNote = catchAsync( async( req, res) => {
     const noteId = req.params.id;
@@ -44,4 +46,36 @@ const getNote = catchAsync( async( req, res) => {
     })
 })
 
-module.exports= { newNotes, getMyNotes, getNote }
+
+const deleteNote = catchAsync( async( req, res ) => {
+
+    const noteId = req.params.id;
+    await deleteaNote( { _id: noteId, owner: req.user } )
+
+    res.status( StatusCodes.OK ).json({
+        status: true,
+        msg: "note deleted successfully!"
+    })
+
+})
+
+
+const updateNote = catchAsync( async( req, res ) => {
+
+    const noteId = req.params.id;
+    const { title, body } = req.body;
+
+    const updatedNote = await updateaNote(
+        noteId,
+        { $set: { title, body } },
+        { new: true, runValidators: true }
+    )
+
+    res.status( StatusCodes.OK ).json({
+        status: true,
+        updatedNote
+    })
+
+})
+
+module.exports= { newNotes, getMyNotes, getNote, deleteNote, updateNote }

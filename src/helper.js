d455f948418204a795cbc748e7e73b01efd831bf
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-17 06:31:55 +0100

    added  a helper file

---
+++ src/helper.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const CONSTANTS = require('./constants/ts')
+require('dotenv').config()
+
+const signToken = ( id ) => {
+    return jwt.sign({ id }, process.env.SECRET_KEY)
+}
+
+const setCookies = ( res, tokenName, token ) => {
+
+    return res.cookie(tokenName, token, {
+        httpOnly: true,
+        secure: CONSTANTS.APP_ENV.PRODUCTION,
+    })
+}
+
+module.exports={ signToken, setCookies }

c0abe6469d0e4d7760864256c714d28eac600c4b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-17 21:21:13 +0100

    addded protect middleware

---
+++ src/middlewares/validate.notes.js
@@ -0,0 +1,17 @@
+const { AppError } = require("../errors/AppError");
+const catchAsync = require("../errors/catchAsync");
+const { getaNote } = require("../services/notes");
+
+const protect = catchAsync( async( req, res, next ) => {
+    const noteId = req.params.id;
+    const owner = req.user;
+    const found = await getaNote({ owner: owner, _id: noteId })
+
+    if (!found){
+        throw new AppError(`note with ID: ${noteId} not found`)
+    }
+    next();
+
+})
+
+module.exports= { protect }

b3de014618d63ef3a4729d2969b4ed1b7b00c86b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-18 20:46:36 +0100

    changed statusCode

--- src/middlewares/authenticate.user.js
+++ src/middlewares/authenticate.user.js
@@ -7,12 +7,12 @@ const validateUser = catchAsync( async( req, res, next ) =>{
 
     const token = req.cookies.jwt_token;
     if (!token){
-        throw new AppError('You are not logged in!', StatusCodes.FORBIDDEN )
+        throw new AppError('You are not logged in!', StatusCodes.UNAUTHORIZED )
     }
     
     const userId = (await decodeToken(token)).id;
     if (!userId) {
-        throw new AppError('Invalid token!', StatusCodes.FORBIDDEN)
+        throw new AppError('Invalid token!', StatusCodes.UNAUTHORIZED )
     }
 
     req.user = userId

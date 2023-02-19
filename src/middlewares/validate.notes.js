9acab9923920b21a8d4e9aa988bfb6d625f5eb77
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-19 07:29:13 +0100

    improve: modified validatenote middleware

--- src/middlewares/validate.notes.js
+++ src/middlewares/validate.notes.js
@@ -17,13 +17,14 @@ const protect = catchAsync( async( req, res, next ) => {
 })
 
 const validatenote = catchAsync( async( req, res, next ) => {
-    const { title, body } = trimString({ ...req.body })
+    
+    const { title: noteTitle, body: noteBody } = trimString({ ...req.body })
 
-    if ( ( !req.body.title && !req.body.body ) ) {
+    if ( !req.body.hasOwnProperty('title') && ! req.body.hasOwnProperty('body') ) {
         throw new AppError('Title or body not provided!', StatusCodes.BAD_REQUEST)
     }
 
-    else if ( !title && !body) {
+    else if ( !noteTitle && !noteBody ) {
         throw new AppError('Empty note not saved!', StatusCodes.BAD_REQUEST)
     }
     next();

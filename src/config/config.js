615110f9db4cc0567409423ef0a06828fc8b74dc
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-19 09:38:04 +0100

    new: addeda config variable

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const DBURL = process.env.NODE_ENV === 'test'? process.env.TEST_DBURL : process.env.DB_URL;
 const PORT = process.env.PORT;
 const SECRET = process.env.SECRET_KEY;
+const APP_ENV = 'production';
 
 module.exports={
     DBURL,
     PORT,
-    SECRET
+    SECRET,
+    APP_ENV
 }

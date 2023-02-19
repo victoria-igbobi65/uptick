4b91c37eee973355bdcd2dc18448540c87a48861
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-19 07:13:06 +0100

    new: added a validate note middleware

--- src/middlewares/validate.notes.js
+++ src/middlewares/validate.notes.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const { AppError } = require("../errors/AppError");
 const catchAsync = require("../errors/catchAsync");
 const { getaNote } = require("../services/notes");
+const { trimString } = require('../helper')
 
 const protect = catchAsync( async( req, res, next ) => {
     const noteId = req.params.id;
@@ -15,4 +16,17 @@ const protect = catchAsync( async( req, res, next ) => {
 
 })
 
-module.exports= { protect }
+const validatenote = catchAsync( async( req, res, next ) => {
+    const { title, body } = trimString({ ...req.body })
+
+    if ( ( !req.body.title && !req.body.body ) ) {
+        throw new AppError('Title or body not provided!', StatusCodes.BAD_REQUEST)
+    }
+
+    else if ( !title && !body) {
+        throw new AppError('Empty note not saved!', StatusCodes.BAD_REQUEST)
+    }
+    next();
+})
+
+module.exports= { protect, validatenote }

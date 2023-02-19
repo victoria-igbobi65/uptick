bba710a48fb25100eab636a10e8b4b7afd3bc2cd
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-19 16:28:09 +0100

    improve: modified middlewares

--- src/app.js
+++ src/app.js
@@ -5,19 +5,22 @@ const logger = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
-const rateLimit = require('express-rate-limit')
+//const rateLimit = require('express-rate-limit')
 const mongoSanitize = require('express-mongo-sanitize')
 const cookieParser = require('cookie-parser')
 const { userRouter } = require('./routes/user')
 const { noteRouter } = require('./routes/notes')
-const { AppError } = require('./errors/AppError')
 const globalErrorhandler = require('./errors/errorHandler')
+const unknownEndpoint = require('./middlewares/unknownEndoint')
+const rootRouter = require('./controllers/root')
+const CONFIG = require('./config/config')
+require('./middlewares/db')( CONFIG.DBURL )
 const app = express()
+
 
 /* MIDDLEWARES */
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
@@ -26,28 +29,14 @@ app.use(cors()) /* allow requests from all origins */
 app.use(helmet())
 app.use(xss()) /* sanitixe user input*/
 app.use( mongoSanitize()) /* sanitize user input to prevent DB operator injection*/
-app.use(
-    rateLimit({
-        windowMs: 12 * 60 * 60 * 1000,
-        max: 5,
-        message: 'You exceeded 100 request in 12 hours!',
-        headers: true,
-    })
-)
-
-
-app.get('/', ( req, res ) =>{
-    res.render('index')
-})
+app.get('/', rootRouter)
 app.use('/api/v1/auth', userRouter)
 app.use('/api/v1/note', noteRouter )
 
 
 
 /* Error handler Middlewares */
-app.use('*', (req, res, next) => {
-    next(new AppError(`Can't find ${req.method} ${req.originalUrl} on this server!`, 404))
-})
+app.use('*', unknownEndpoint)
 app.use(globalErrorhandler)
 
 module.exports= { app }

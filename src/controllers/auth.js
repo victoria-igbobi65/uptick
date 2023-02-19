c7febce0383c8fe196ef813e8e5776c3a62a96c3
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-19 09:43:31 +0100

    new: replace config variables

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,7 +3,6 @@ const { StatusCodes } = require('http-status-codes')
 const { getaUser, createUser } = require('../services/user')
 const { AppError } = require('../errors/AppError')
 const { signToken, setCookies } = require('../helper')
-const CONSTANTS = require('../constants/ts')
 const catchAsync = require('../errors/catchAsync')
 
 
@@ -13,7 +12,7 @@ const registration = catchAsync( async( req, res ) => {
     const found = await getaUser({ email: email })
 
     if (found){
-        throw new AppError( CONSTANTS.MESSAGE.ERROR.ALREADY_EXISTS, StatusCodes.CONFLICT )
+        throw new AppError( 'user with this email already exists', StatusCodes.CONFLICT )
     }
     const newUser = await createUser({ email, password })
     newUser.password = undefined;
@@ -30,10 +29,10 @@ const login = catchAsync( async( req, res ) => {
     const user = await getaUser({ email: email })
 
     if ( !user || !( await user.correctPassword(password, user.password ))){
-        throw new AppError( CONSTANTS.MESSAGE.ERROR.LOGIN, StatusCodes.BAD_REQUEST )
+        throw new AppError( 'email or password incorrect!', StatusCodes.BAD_REQUEST )
     }
     const token = signToken( user._id)
-    setCookies(res, CONSTANTS.TOKEN.NAME, token)
+    setCookies(res, 'jwt_token', token)
 
     res.status( StatusCodes.OK ).json({
         msg: "Login successful!"

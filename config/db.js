0c988fefbcf0fadccdcc3f7efcff88bc82637dce
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-20 18:58:26 +0100

    new: moved file into config folder

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+module.exports = (URI) => {
+
+    mongoose.set('strictQuery', false)
+    mongoose
+        .connect(URI)
+        .then(() => {
+            console.log('Connection to MongoDB successful')
+        })
+        .catch((err) => {
+            console.error('Connection to MongoDB failed', err.message)
+        })
+}

419b61efff98e4b12810412c0094c678ef8be79a
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-21 20:45:04 +0100

    improve: refactored test suites

--- test/createNote.test.js
+++ test/createNote.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const mongoose = require('mongoose')
+const HELPER = require('./testhelper')
 const { app } = require('../app')
 const { userModel } = require('../src/models/user')
 const { noteModel } = require('../src/models/note')
@@ -12,14 +13,9 @@ describe('Create new note', () => {
         await userModel.deleteMany({})
         await noteModel.deleteMany({})
 
-        const newUser = {
-            email: 'userone@gmail.com',
-            password: 'password',
-        }
-
-        await request(app).post('/api/v1/auth/signup').send( newUser )
+        await request(app).post('/api/v1/auth/signup').send( HELPER.user )
       
-        const loginResponse = await request(app).post('/api/v1/auth/login').send(newUser)
+        const loginResponse = await request(app).post('/api/v1/auth/login').send( HELPER.user )
         authToken = loginResponse.headers['set-cookie'][0].split(';')[0]
         
     })
@@ -29,29 +25,22 @@ describe('Create new note', () => {
     })
 
     test('returns a 201 if new note was created successfully', async () => {
-        const note = {
-            title: 'Fall of the ocean',
-            body: '',
-        }
+       
         const response = await request(app)
             .post('/api/v1/note')
             .set("Cookie", authToken)
-            .send(note)
+            .send( HELPER.note )
         expect( response.statusCode ).toBe( 201 )
         expect( response.body.newNotes ).toHaveProperty('title')
-        expect(response.body.newNotes.title).toBe('Fall of the ocean')
+        expect(response.body.newNotes.title).toBe('Fall of the Ocean')
         expect( response.body.msg).toBe( 'Notes created successfully!')
     })
 
     test('should return a 401 error if cookie not provided', async() => {
 
-        const note = {
-            title: 'Fall of the ocean',
-            body: '',
-        }
-        const response = await request(app)
+        const response = await request( app )
             .post('/api/v1/note')
-            .send(note)
+            .send( HELPER.note )
         expect( response.statusCode ).toBe(401)
         expect( JSON.parse(response.text).status ).toBe('fail')
         expect( JSON.parse(response.text).message ).toBe('You are not logged in!')
@@ -60,13 +49,10 @@ describe('Create new note', () => {
 
     test('should return a 400 error when no data is passed', async() => {
 
-        const note = {
-           
-        }
         const response = await request(app)
             .post('/api/v1/note')
             .set('Cookie', authToken)
-            .send(note)
+            .send({})
         expect( response.statusCode ).toBe(400)
         expect( response.body ).toHaveProperty('status')
         expect( response.body ).toHaveProperty('message')
@@ -77,14 +63,10 @@ describe('Create new note', () => {
 
     test('should return a 400 error when title and body is empty', async () => {
 
-        const note = {
-            title: '',
-            body: '',
-        }
         const response = await request(app)
             .post('/api/v1/note')
             .set('Cookie', authToken)
-            .send(note)
+            .send( HELPER.emptyNote )
         expect(response.statusCode).toBe(400)
         expect(JSON.parse(response.text).status).toBe('fail')
         expect(JSON.parse(response.text).message).toBe('Empty note not saved!')  

1906d6293f338e0e43cae6d1737dbfd4d7dade15
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-21 20:29:32 +0100

    new: helper function for tests

---
+++ test/testhelper.js
@@ -0,0 +1,11 @@
+
+exports.invalidId = '63f5022ff0b2c6f5c4cf5fb9';
+exports.user = {
+    email: 'userone@gmail.com',
+    password: 'password',
+}
+exports.note = {
+    title: 'Fall of the Ocean',
+    body: 'Hearld grift thoughts',
+}
+

4f02172624fa89ac0d14d1c2792be14c241f416b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-22 06:57:26 +0100

    improve: logged info nicely

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ var { logger } = require('./src/utils/logger')
 var CONFIG = require('./config/config')
 var PORT = CONFIG.PORT || 3000;
 
-/* starting server */
+/*Uncaught exception*/
+process.on('uncaughtException', (err) => {
+    logger.error('UNCAUGHT EXCEPTION! ...Shutting down....')
+    logger.error(`Error: ${ err.message }`)
+    process.exit(1)
+})
+
+
 var server = app.listen(PORT, () => {
     logger.info(`Server is running on http://localhost:${ PORT }`);
 });
@@ -11,7 +18,8 @@ var server = app.listen(PORT, () => {
 
 //HANDLING UNHANDLED REJECTIONS
 process.on('unhandledRejection', err => {
-    logger.info('Unhandled Rejection! ...Shutting down....')
+    logger.error('Unhandled Rejection! ...Shutting down....')
+    logger.error(`Error: ${ err}`)
     server.close(() => {
         process.exit(1);
     }); 

8c59a5dee7b7fb02443131dc6ca1c4c2fadfe58a
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-22 06:56:19 +0100

    new: added request logger middleware, removed unused imports

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 /* Module Imports */
 const express = require('express')
 const path = require('path')
-const logger = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
-//const rateLimit = require('express-rate-limit')
 const mongoSanitize = require('express-mongo-sanitize')
 const cookieParser = require('cookie-parser')
 const { userRouter } = require('./src/routes/user')
 const { noteRouter } = require('./src/routes/notes')
+const requestLoggerMiddleware = require('./src/utils/requestLogger')
 const globalErrorhandler = require('./src/errors/errorHandler')
 const unknownEndpoint = require('./src/middlewares/unknownEndoint')
 const rootRouter = require('./src/controllers/root')
@@ -28,12 +27,10 @@ app.use(cors()) /* allow requests from all origins */
 app.use(helmet())
 app.use(xss()) /* sanitixe user input*/
 app.use( mongoSanitize()) /* sanitize user input to prevent DB operator injection*/
+app.use(requestLoggerMiddleware) /* middleware to log requests depending on the environment*/
 app.get('/', rootRouter)
 app.use('/api/v1/auth', userRouter)
 app.use('/api/v1/note', noteRouter )
-if ( CONFIG.NODE_ENV !== 'test'){
-    app.use(logger('dev'))
-}
 
 
 

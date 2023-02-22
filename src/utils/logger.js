93e9c8b28552a9bbf671c12655d92da1e91c4ae6
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-22 07:00:02 +0100

    improve: removed logging info to logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,18 +1,28 @@
 const winston = require('winston')
-const CONFIG = require('../../config/config')
+const path = require('path')
 
-const logger = CONFIG.NODE_ENV === 'test'
-    ? winston.createLogger({ silent: true})
-    : winston.createLogger({
-        transports: [
-            new winston.transports.Console({
-                level: 'info',
-                format: winston.format.combine(
-                    winston.format.colorize(),
-                    winston.format.simple()
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.printf(
+                    (info) => `${info.timestamp} ${info.message}`
                 )
-            })
-        ]
-    })
+            ),
+            silent: process.env.NODE_ENV === 'test' ? true : false,
+        }),
+        new winston.transports.File({
+            filename: path.join(__dirname, '../', '../logs/error.log'),
+            level: 'error',
+        }),
+        
+    ],
+})
 
 module.exports = { logger }

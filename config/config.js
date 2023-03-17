dc4f3f4b576238eecb1d44574074e8f12dc52e1a
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-17 20:07:36 +0100

    improve: added trebble config

--- config/config.js
+++ config/config.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT;
 const SECRET = process.env.SECRET_KEY;
 const NODE_ENV = process.env.NODE_ENV;
 const APP_ENV = 'production';
+const TREBLLE_KEY = process.env.TREBLLE_API_KEY;
+const TREBLLE_PROJECT_ID = process.env.TREBLLE_PROJECT_ID;
 
 module.exports={
     DBURL,
     PORT,
     SECRET,
     APP_ENV,
-    NODE_ENV
+    NODE_ENV,
+    TREBLLE_KEY,
+    TREBLLE_PROJECT_ID
 }

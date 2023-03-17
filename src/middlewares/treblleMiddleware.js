3d78f064a0726ff5266de2e237d94d1b7ff95d39
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-17 20:45:35 +0100

    new: treblle middleware

--- src/middlewares/treblleMiddleware.js
+++ src/middlewares/treblleMiddleware.js
@@ -1,7 +1,9 @@
-const treblle = require('@treblle/express')
 const CONFIG = require('../../config/config')
 
-module.exports = treblle({
+const tMiddleware = {
+
     apiKey: CONFIG.TREBLLE_KEY,
     projectId: CONFIG.TREBLLE_PROJECT_ID
-})
+}
+
+module.exports = { tMiddleware }
